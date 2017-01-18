4f1454c4ec230c25026926f9700be566696ad5df
Edward Dam <edward.dam@gmail.com>
2017-01-18 22:56:45 +0000

    Initial Commit

---
+++ src/pkjs/config.js
@@ -0,0 +1,33 @@
+// Author: Ed Dam
+
+module.exports = [
+  {
+    "type": "heading",
+    "defaultValue": "Rainfall v1.0"
+  },
+  {
+    "type": "text",
+    "defaultValue": "by Edward Dam"
+  },
+  {
+    "type": "section",
+    "items": [
+      {
+        "type": "heading",
+        "defaultValue": "Powered by Dark Sky"
+      },
+      {
+        "type": "text",
+        "defaultValue": "https://darksky.net/poweredby/"
+      },
+    ]
+  },
+  {
+    "type": "text",
+    "defaultValue": "Thank you for using my watch app."
+  },
+  {
+    "type": "submit",
+    "defaultValue": "Exit"
+  }
+];
